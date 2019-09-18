d99badce3e26caf8e2b84e0d81d1564b7ce66359
Valeria <daukshislera2000@gmail.com>
2019-09-18 11:58:21 +0300

    first steps

---
+++ SPP_lab1/SPP_testThreads/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SPP_testThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StackTrace trace = new StackTrace(1, true);
+            A.method1();
+            var a = new StackTrace().GetFrame(0).GetMethod();
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+
+    static class A
+    {
+        public static void method1()
+        {
+            StackTrace trace = new StackTrace(1, true);
+            int a = trace.FrameCount;
+            for (int i = 0; i < a; i++)
+                Console.WriteLine(trace.GetFrame(i).GetMethod().Name);
+            method2();
+        }
+
+        public static void method2()
+        {
+            Console.WriteLine("******");
+            StackTrace trace = new StackTrace(1, true);
+            int a = trace.FrameCount;
+            for (int i = 0; i < a; i++)
+                Console.WriteLine(trace.GetFrame(i).GetMethod().Name);
+            Console.WriteLine("******");
+        }
+    }
+
+    
+}
---
+++ SPP_lab1/SPP_testThreads/ThreadInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPP_testThreads
+{
+    public class ThreadInfo
+    {
+        private readonly int _id;
+        internal long _time;
+        internal List<MethodInfo> methodInfo;
+        
+
+        public ThreadInfo(int id)
+        {
+            _id = id;
+            _time = 0;
+            methodInfo = new List<MethodInfo>(); 
+        }
+    }
+}
